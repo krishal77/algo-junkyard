c1ca8f4bb83ece425bfb2b62ae49ba28af7179a7
krishal77 <kthapaliya77@gmail.com>
2026-06-09 22:40:53 +0545

    vanya and fence

---
+++ problems/vanya_fence.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    int n,h;
+    int width=0;
+    cin>>n>>h;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]<=h){
+            width+=1;
+        }
+        else{
+            width+=2;
+        }
+    }
+    cout<<width;
+}
