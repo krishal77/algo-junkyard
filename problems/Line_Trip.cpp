6bc2bea7a35531083082392ccf52431c439909dc
krishal77 <kthapaliya77@gmail.com>
2026-06-12 22:01:10 +0545

    line_trip

---
+++ problems/Line_Trip.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    while (n--) {
+
+        int y, a;
+        cin >> y >> a;
+
+        int fuel[y];
+
+        for (int i = 0; i < y; i++) {
+            cin >> fuel[i];
+        }
+
+        int k = 0;
+        int maxFuel = 0;
+
+
+        for (int i = 0; i < y - 1; i++) {
+
+            if ((fuel[i + 1] - fuel[i]) > maxFuel) {
+                maxFuel = fuel[i + 1] - fuel[i];
+            }
+        }
+
+        if ((fuel[0] - 0) > maxFuel) {
+            maxFuel = fuel[0];
+        }
+     if ((a - fuel[y - 1]) * 2 > maxFuel) {
+            maxFuel = (a - fuel[y - 1]) * 2;
+        }
+
+        cout << maxFuel << endl;
+    }
+
+    return 0;
+}
