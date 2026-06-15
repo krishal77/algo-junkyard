f9f3a642c42e3f7c555e23567068a6481b416123
krishal77 <kthapaliya77@gmail.com>
2026-06-15 17:31:36 +0545

    lucky

---
+++ problems/lucky.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t>0){
+    string n;
+    cin>>n;
+    int suma=0,sumb=0;
+   for(int i=0;i<3;i++){
+    suma=suma+n[i];
+   }
+    for(int i=5;i>=3;i--){
+    sumb=sumb+n[i];
+   }
+
+   if(suma==sumb){
+    cout<<"YES";
+   }
+   else{
+    cout<<"NO";
+   }
+   t--;
+}
+}
