cb71f0344a16fd6860d077f565ffcedd13e38cfc
krishal77 <kthapaliya77@gmail.com>
2026-06-21 23:17:16 +0545

    complete

--- problems/two_vessels.cpp
+++ problems/two_vessels.cpp
@@ -2,17 +2,25 @@
 using namespace std;
 
 int main(){
+    int t;
+    cin>>t;
+    while(t>0){
     int a,b,c;
     cin>>a>>b>>c;
-    int avg=0,moo=0;
+    int avg=0,moves=0;
     float mx,difference;
     avg=(a+b)/2;
     mx=max(a,b);
     difference= abs(mx-avg);
+    while(difference>0){
+        difference=difference-c;
+        moves++;
+    }
 
-    cout<<moo<<endl;
-
-
+    
+cout<<moves<<endl;
+t--;
+    }
 
 
 }
