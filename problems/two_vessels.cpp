6fb764f1648f506a3bb88c10af8f278c00b1661c
krishal77 <kthapaliya77@gmail.com>
2026-06-21 23:12:48 +0545

    not finished

---
+++ problems/two_vessels.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    int avg=0,moo=0;
+    float mx,difference;
+    avg=(a+b)/2;
+    mx=max(a,b);
+    difference= abs(mx-avg);
+
+    cout<<moo<<endl;
+
+
+
+
+}
