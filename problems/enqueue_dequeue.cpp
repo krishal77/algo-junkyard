07824e53ff18065aa988520661a6bfdcd4a85ac4
krishal77 <kthapaliya77@gmail.com>
2026-06-22 10:11:57 +0545

    Queue using array

---
+++ problems/enqueue_dequeue.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+#define SIZE 2
+
+int A[SIZE];
+int front = -1, rear = -1;
+
+bool isEmpty()
+{
+    return (front == -1 && rear == -1);
+}
+
+void enqueue(int val)
+{
+    if (rear == SIZE - 1)
+    {
+        cout << "Queue is full" << endl;
+        return;
+    }
+
+    if (front == -1)
+    {
+        front = 0;
+    }
+
+    rear++;
+    A[rear] = val;
+    cout << "Enqueued Value : " << val << endl;
+}
+
+void dequeue()
+{
+    if (isEmpty())
+    {
+        cout << "Queue is empty" << endl;
+        return;
+    }
+
+    if (front == rear)
+    {
+        front = rear = -1;
+    }
+    else
+    {
+        if (front == rear)
+        {
+            front = rear = -1;
+        }
+        else
+        {
+            cout << "Dequeued Value : " << A[front] << endl;
+            front++;
+        }
+    }
+}
+
+
+int main()
+{
+    dequeue();
+    enqueue(2);
+    enqueue(4);
+    enqueue(6);
+    dequeue();
+
+    return 0;
+}
