d260ddc366ca044f34b2aee5984b4ac95f2c3221
krishal77 <kthapaliya77@gmail.com>
2026-06-25 22:06:22 +0545

    little tanya

---
+++ problems/little_tanya.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int num,k;
+    cin>>num;
+    cin>>k;
+    while(k--){
+        if(num%10==0){
+            num=num/10;
+        }
+        else{
+          num--;
+        }
+    }
+    cout<<num;
+}
