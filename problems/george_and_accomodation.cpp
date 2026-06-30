b40fc6c74a05ce2d0c491546789992c103904fe1
krishal77 <kthapaliya77@gmail.com>
2026-06-30 22:43:53 +0545

    room

---
+++ problems/george_and_accomodation.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    int count=0;
+    while(t--){
+    int a, b;
+    cin>>a>>b;
+    
+    if(((b-a))>=2){
+        count++;
+    }
+    
+    }
+    cout<<count;
+}
