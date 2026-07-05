e630dbb073bb44b8b9dd6ac90c5bcf6517269a64
krishal77 <kthapaliya77@gmail.com>
2026-07-05 22:09:49 +0545

    conversion

---
+++ problems/infix_to_postfix.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int priority( char op)
+{
+    if(op =='^'){
+        return 3;
+    }
+    else if(op == '*' || op== '/'){
+        return 2;
+    }
+    else if( op == '+' || op == '-'){
+        return 1;
+    }
+    else {
+        return -1;
+    }
+}
+
+string infixToPostfix(string s){
+stack <char> st;
+int n=s.length();
+int i=0;
+string ans="";
+while(i<n){
+    if((s[i]>='a' && s[i]<='z')||
+      (s[i]>='A' && s[i]<='Z')||
+      (s[i]>='0' && s[i]<='9') )
+      {
+            ans=ans+s[i];
+      }
+      else if(s[i]=='('){
+            st.push(s[i]);
+      }
+       else if(s[i]==')'){
+            while(!st.empty() && st.top()!='('){
+                ans+=st.top();
+                st.pop();
+            }
+            st.pop();
+      }
+      else{
+        while(!st.empty() && st.top() != '(' && priority(s[i])<= priority(st.top())){
+            ans+=st.top();
+            st.pop();
+        }
+        st.push(s[i]);
+      }
+      i++;
+}
+while(!st.empty())
+{
+    ans=ans+st.top();
+    st.pop();
+}
+return ans;
+}
+
+int main(){
+string express;
+cin>>express;
+
+string postfix= infixToPostfix(express);
+cout << postfix << endl;
+}
