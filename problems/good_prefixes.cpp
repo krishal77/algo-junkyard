77d0ff5c84071ade327bae5e6f08018c96dd02f1
krishal77 <kthapaliya77@gmail.com>
2026-07-08 23:36:46 +0545

    fixed TC

--- problems/good_prefixes.cpp
+++ problems/good_prefixes.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
-#include <unordered_map>
 using namespace std;
 
 int main() {
     int t;
     cin >> t;
- while (t--) {
+
+    while (t--) {
         int n;
         cin >> n;
 
         long long sum = 0;
+        long long mx = 0;
         int ans = 0;
-        unordered_map<long long, int> moves;
 
         for (int i = 0; i < n; i++) {
             long long x;
             cin >> x;
 
             sum += x;
-            moves[x]++;
+            mx = max(mx, x);
 
-            if (sum%2 == 0 && moves.count(sum / 2))
+            if (sum == 2 * mx)
                 ans++;
         }
+
         cout << ans << endl;
     }
 }
