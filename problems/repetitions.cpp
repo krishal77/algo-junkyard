2d9f7b541e8e31dcff2fde49988972b308aa5714
krishal77 <kthapaliya77@gmail.com>
2026-07-13 17:22:19 +0545

    CSES#3

---
+++ problems/repetitions.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    int ans=1, count=0;
+    char l='A';
+    for(char d:s){
+        if(d==l){
+            ++count;
+            ans=max(count,ans);
+        }
+        else{
+            l=d;
+            count=1;
+
+        }
+    }
+    cout<<ans;
+
+}
